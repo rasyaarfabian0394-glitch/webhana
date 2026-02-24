09c59a35ef5f100a7167c95a2af4bc6e132444f9
DUI HANDOKO <rasyaarfabian0394@gmail.com>
2026-02-25 06:00:30 +0700

    Update login.js

--- login.js
+++ login.js
@@ -4,10 +4,15 @@ let user = document.getElementById("username").value;
 let pass = document.getElementById("password").value;
 
 if(user == "HanaStoreOne" && pass == "Hana2203"){
-
 window.location = "home.html";
 
-}else{
+}
+  
+else if(user == "Rasya01" && pass == "Hana2203"){
+window.location = "home.html";
+}
+  
+else{
 
 document.getElementById("pesan").innerHTML = "Username atau Password salah";
 
