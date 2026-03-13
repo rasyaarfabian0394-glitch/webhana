72bc3d0c2015ef219a61e34c83ec0d0a82887962
DUI HANDOKO <rasyaarfabian0394@gmail.com>
2026-03-14 00:34:50 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,9 @@
 <?php
+
 $port = getenv("PORT") ?: 8080;
 
-$command = "php -S 0.0.0.0:$port";
-passthru($command);
+echo "Server berjalan di port ".$port.PHP_EOL;
+
+passthru("php -S 0.0.0.0:$port");
+
+?>
